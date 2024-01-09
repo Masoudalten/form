15d189d37e681878c03e8019eb31b07ad4ecca6c
PROD\mbahmanyar <masoud.bahmanyar@alten.it>
2024-01-09 16:02:31 +0100

    First commit: template-form created:

---
+++ template-driven/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'template-driven';
+
+  @ViewChild('registrationForm') form: NgForm;
+
+
+  OnFormSubmitted() {
+    console.log(this.form);
+
+  }
+}
